5ed2b3d2d73895d0f63e08d21a2a905eff7d4721
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2022-11-30 04:30:22 -0300

    cardMovie/Ajusts

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -4,8 +4,8 @@ function Form() {
 
     const movie = {
         id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
+        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/pz7bGOUNJXyPKJ2GcJN6nhwBnCs.jpg",
+        title: "A Fera",
         count: 2,
         score: 4.5
     };
