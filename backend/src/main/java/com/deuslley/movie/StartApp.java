26aa801574b057adddcb93b8980981dcdc79f9ba
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2022-12-05 03:52:05 -0300

    Security_Config/BcEnd

--- backend/src/main/java/com/deuslley/movie/StartApp.java
+++ backend/src/main/java/com/deuslley/movie/StartApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MovieApplication {
+public class StartApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MovieApplication.class, args);
+		SpringApplication.run(StartApp.class, args);
 	}
 
 }
