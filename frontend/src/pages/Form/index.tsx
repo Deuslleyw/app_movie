39d13a238756190a08c77f983ef10efc0500e080
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2023-01-16 03:54:19 -0300

    update/Forms

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,49 +1,17 @@
-import { Link } from 'react-router-dom';
-import { Movie } from 'Types/movie';
-import './styles.css'
+import FormCard from 'components/FormCard';
+import {  useParams } from 'react-router-dom';
 
 
-type Props = { movie : Movie;
-
-}
-
-
-
-function Form( {movie} : Props)
+function Form()
 {
        
+    const params = useParams();
+
     return (
 
-        <div className="movie-form-container">
-            <img className="movie-movie-card-image" src={movie.image} alt={movie.title} />
-            <div className="movie-card-bottom-container">
-                <h3>{movie.title}</h3>
-                <form className="movie-form">
-                    <div className="form-group movie-form-group">
-                        <label htmlFor="email" >Informe seu email</label>
-                        <input type="email" className="form-control" id="email" />
-                    </div>
-                    <div className="form-group movie-form-group">
-                        <label htmlFor="score">informe sua avaliação</label>
-                        <select className="form-control" id="score">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                        </select>
-                    </div>
-                    <div className="movie-form-btn-container">
-                        <button type="submit" className="btn btn-primary movie-btn"> Salvar</button>
-                    </div>
-                </form>
-                <Link to="/">
-                    <button className="btn btn-primary movie-btn mt-3">Cancelar</button>
-                </Link>
-            </div>
-        </div>
+        <FormCard movieId={`${params.movieId}`} />
 
-    )
+    );
 }
 
 export default Form;
